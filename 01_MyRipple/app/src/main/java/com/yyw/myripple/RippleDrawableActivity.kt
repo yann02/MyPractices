a1ec97e0522d74ad0186bc35702634dbca01ebcb
yingyanwu <1179858356@qq.com>
2021-09-15 07:59:00 +0800

    练习点击水波纹效果

---
+++ 01_MyRipple/app/src/main/java/com/yyw/myripple/RippleDrawableActivity.kt
@@ -0,0 +1,11 @@
+package com.yyw.myripple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RippleDrawableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ripple_drawable)
+    }
+}
