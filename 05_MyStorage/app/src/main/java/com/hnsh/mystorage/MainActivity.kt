d3761d0247afce5600b1cd128a3bb8926b2b8693
yingyanwu <1179858356@qq.com>
2021-11-05 18:49:51 +0800

    开发本地缓存的功能

--- 05_MyStorage/app/src/main/java/com/hnsh/mystorage/MainActivity.kt
+++ 05_MyStorage/app/src/main/java/com/hnsh/mystorage/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.hnsh.mystorage
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val path = filesDir.absolutePath
         Log.d("wyy", "path:$path")
+        val file = File(path + File.separator + "myFile")
+//        if (file.isDirectory) {
+//            Log.d("wyy", "File is a directory.")
+//            Log.d("wyy", "File has ${file.list()?.size ?: 0} files.")
+//        val fileName = "myFile"
+//            val fileContent = "我是好人，不要打我"
+//            openFileOutput(fileName, Context.MODE_PRIVATE).bufferedWriter(Charsets.UTF_8).use {
+//                it.write(fileContent)
+//            }
+//        }
+//        openFileInput(fileName).bufferedReader().useLines {
+//            it.fold("1") { some, text ->
+//                Log.d("wyy","some:$some")
+//                Log.d("wyy","text:$text")
+//                "$some\n$text"
+//            }
+//            Log.d("wyy", "it:${it}")
+//        }
+        resources.openRawResource(R.raw.abc).bufferedReader().use {
+            it.forEachLine { s ->
+                Log.d("wyy", "it:$s")
+            }
+        }
+        val path2 = externalCacheDir?.absolutePath
+        Log.d("wyy", "path2:$path2")
     }
 }
